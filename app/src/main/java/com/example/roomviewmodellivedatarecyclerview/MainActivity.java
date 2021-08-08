f46b670024b63974f4866d12e6b2fa527d344961
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2021-08-07 20:21:11 -0400

    viewholder, Recyclerview, CardView, Adapters,Databse connectivity to RecyclerView.

--- app/src/main/java/com/example/roomviewmodellivedatarecyclerview/MainActivity.java
+++ app/src/main/java/com/example/roomviewmodellivedatarecyclerview/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.roomviewmodellivedatarecyclerview;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+//        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                adapter.setNotes(notes);
+            }
+        });
     }
 }
+
+
